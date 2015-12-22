6da92e3a85353d26118d55bc1b619ea110699639
johnny <johnny.niu@hotmail.com>
2015-12-23 10:55:08 +1100

    change api urls

--- monitor/http/sensorHttpService.py
+++ monitor/http/sensorHttpService.py
@@ -6,14 +6,14 @@ class Status:
 	occupied = 'occupied'
 	vacant='vacant'
 
-base_url= "http://52.62.29.150:8080/ts/"
+base_url= "http://52.62.29.150:8080/level/4/room/1/male/slot/"
 
 def sendReq(tid, status):
     emptyData = urllib.parse.urlencode({}).encode('utf8')
     url = base_url + str(tid) + "/" + status
     req = urllib.request.Request(url, data=emptyData,   headers={'Content-Type': 'application/json'})
     resp = urllib.request.urlopen(req);
-    print ("sending http request status as :  ", status)
+    print ("sending http request :  ", url)
 
 def setOccupied(tid):
   	sendReq(tid, Status.occupied)
